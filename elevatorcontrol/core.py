24707cb400c447ddf9493638a08e204a5052c3af
mridul <mridulkapoor@gmail.com>
2016-11-06 22:58:54 -0500

    empty implementation of elevator control system

---
+++ elevatorcontrol/core.py
@@ -0,0 +1,12 @@
+class ElevatorControlSystem(object):
+    def __init__(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def update_state(self, elevator_id, floor_num, goal_floor):
+        pass
+
+    def pickup_request(self, pickup_from, direction):
+        pass
