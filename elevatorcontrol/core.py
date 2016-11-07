46e2c92a682771b21d06a48e3e57f4925d1c0a8d
mridul <mridulkapoor@gmail.com>
2016-11-06 23:12:44 -0500

    create basic Elevator class

--- elevatorcontrol/core.py
+++ elevatorcontrol/core.py
@@ -1,9 +1,24 @@
+import itertools
+
+
+class Elevator(object):
+    new_elevator_id = itertools.count()
+
+    def __init__(self, floor_num, goal_floor):
+        self.id = next(Elevator.new_elevator_id)
+        self.floor_num = floor_num
+        self.goal_floor = goal_floor
+
+    def state(self):
+        return self.id, self.floor_num, self.goal_floor
+
+
 class ElevatorControlSystem(object):
-    def __init__(self):
-        pass
+    def __init__(self, elevators):
+        self.elevators = elevators
 
     def get_state(self):
-        pass
+        return [elevator.state for elevator in self.elevators]
 
     def update_state(self, elevator_id, floor_num, goal_floor):
         pass
