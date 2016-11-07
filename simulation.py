ed1abcd1fa2ef84b8d97da56923e5dea5e6ba9e4
mridul <mridulkapoor@gmail.com>
2016-11-07 01:59:30 -0500

    adding a very simple logging configuration

--- simulation.py
+++ simulation.py
@@ -1,11 +1,17 @@
+import logging
+import sys
+
 from elevatorcontrol.core import Elevator, ElevatorControlSystem
+
 
 def print_state(states):
     for state in states:
         print("Elevator {} is at {}, and has goals {}".format(state[0], state[1], state[2]))
     print()
 
+
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     # assuming 2 elevators, 10 floors numbered from 1 to 10
     elevators = [Elevator(1, []) for i in range(2)]
     ecs = ElevatorControlSystem(elevators)
